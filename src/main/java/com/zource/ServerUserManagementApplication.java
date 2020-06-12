2c3a1aa2387c36e349fdae5363b2cd9d80e5c620
Mike <sigachev.m@gmail.com>
2020-06-11 20:53:21 -0400

    June 11

--- src/main/java/com/zource/ServerUserManagementApplication.java
+++ src/main/java/com/zource/ServerUserManagementApplication.java
@@ -2,10 +2,11 @@ package com.zource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class ServerUserManagementApplication {
+public class ServerUserManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerUserManagementApplication.class, args);
--- src/main/java/com/zource/config/Swagger2UiConfiguration.java
+++ src/main/java/com/zource/config/Swagger2UiConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class Swagger2UiConfiguration extends WebMvcConfigurerAdapter
 {
 
